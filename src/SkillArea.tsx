51951ee57417b6ffd687a2401203b6c40eab0d3f
Jeremy Badger <jeremebadger@gmail.com>
2024-06-04 17:14:36 -0700

    Removed packages

--- src/SkillArea.tsx
+++ src/SkillArea.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import cx from 'classnames'
-import { BrowserRouter, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import {
   sectionBodyBorderClassNames,
   sectionBodyClassNames,
