0d0d3a7df410e9011043e726e7d3fdef488cbf5c
Jeremy Badger <jeremebadger@gmail.com>
2024-06-05 12:17:17 -0700

    Changed margin on Tech Skills

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -28,7 +28,7 @@ export function Skills({
           )
 
   return (
-    <section className="grid grid-cols-12 mt-5 mb-2">
+    <section className="grid grid-cols-12 mt-5 mb-2 test">
       <p className={cx('font-bold tracking-widest', sectionTitleClassNames)}>
         <span className="font-bold text-blue-900">
           {showTitle ? 'Technical Proficiencies' : null}
@@ -41,12 +41,14 @@ export function Skills({
           sectionBodyBorderClassNames
             .split(' ')
             .map((className) => `print:${className}`)
-        )}>
+        )}
+      >
         <div className="mb-4 flex flex-wrap">
           {sortedSkills.map((skill) => (
             <div
               key={`print-${skill.tech}`}
-              className="w-1/2 sm:w-1/3 md:w-1/4 h-7">
+              className="w-1/2 sm:w-1/3 md:w-1/4 h-7"
+            >
               {skill.tech} <sub>({Math.round(skill.years)} years)</sub>
             </div>
           ))}
